89211a012daafecd517811090506fef844ef10c1
Alexander Myltsev <a.myltsev@gmail.com>
2024-01-08 21:00:52 +0400

    Process all fields, store sheet/row reference.

--- index.py
+++ index.py
@@ -1,8 +1,8 @@
 from whoosh.index import create_in
-from whoosh.fields import Schema, TEXT
+from whoosh.fields import Schema, TEXT, NUMERIC, STORED
 from whoosh.filedb.filestore import RamStorage
 
-from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser
 
 class WhooshReader:
     def __init__(self, index):
@@ -10,24 +10,34 @@ class WhooshReader:
 
     def search(self, query_str):
         with self.index.searcher() as searcher:
-            query = QueryParser("content", self.index.schema).parse(query_str)
+            query = MultifieldParser(["content", "birth_year", "locality"],
+                                     self.index.schema).parse(query_str)
             results = searcher.search(query)
-            return [hit["content"] for hit in results]
+            return [f'{hit["locality"]}, лист {hit["sheet"]}, строка {hit["row"]}: {hit["content"]} / {hit["birth_year"]}' for hit in results]
 
 
 class WhooshWriter:
     def __init__(self):
         # Define the schema for the document
-        schema = Schema(content=TEXT(stored=True))
+        schema = Schema(
+            content=TEXT(stored=True),
+            locality=TEXT(stored=True),
+            birth_year=NUMERIC(stored=True),
+            sheet=STORED,
+            row=STORED,
+        )
         # Create an in-memory index
         self.storage = RamStorage()
         self.index = self.storage.create_index(schema)
+        self.writer = self.index.writer()
 
-    def add_document(self, content):
-        writer = self.index.writer()
-        writer.add_document(content=content)
-        writer.commit()
+    def add_document(self, sheet, row, content, locality, birth_year):
+        if birth_year.isdigit():
+            self.writer.add_document(sheet=sheet, row=row, content=content, locality=locality, birth_year=birth_year)
+        else:
+            self.writer.add_document(sheet=sheet, row=row, content=content, locality=locality)
 
     def close(self):
+        self.writer.commit()
         self.index.close()
 
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from index import WhooshReader, WhooshWriter
@@ -8,29 +10,79 @@ SERVICE_ACCOUNT_FILE = 'genealogy-indexer-8eb0ba824c9a.json'
 # Define the scope
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
-# Authenticate and create the service
-creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-service = build('sheets', 'v4', credentials=creds)
+KATINO = '1_OTKGvCBKHvMsHiz2saVZInPXyyWjAgRc_YX5I7Vrzs'
 
-# Example: Read data from a sheet
-spreadsheet_id = '1_OTKGvCBKHvMsHiz2saVZInPXyyWjAgRc_YX5I7Vrzs'
-range_name = '721!M2:M436'
-sheet = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
-values = sheet.get('values', [])
 
-writer = WhooshWriter()
+class Sheets:
+    def __init__(self, account_file):
+        # Authenticate and create the service
+        creds = service_account.Credentials.from_service_account_file(
+                SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+        self.service = build('sheets', 'v4', credentials=creds)
 
-if not values:
-    print('No data found.')
-else:
-    for row in values:
-        print('adding document:', row[0])
-        writer.add_document(row[0])
+    def get_document(self, spreadsheet_id):
+        return Spreadsheet(self.service, spreadsheet_id)
 
-writer.close()
+class Spreadsheet:
+    def __init__(self, service, spreadsheet_id: str):
+        self.service = service
+        self.spreadsheet_id = spreadsheet_id
+        self.sheet_metadata = self.service.spreadsheets().get(spreadsheetId=spreadsheet_id).execute()
 
-reader = WhooshReader(writer.index)
-search_results = reader.search("Демид")
-for result in search_results:
-    print(result)
+    def get_sheet_titles(self) -> [str]:
+        sheets = self.sheet_metadata.get('sheets', '')
+        return [sheet.get("properties", {}).get("title", "Untitled") for sheet in sheets]
+
+    def get_sheet_content(self, sheet_title: str) -> [[str]]:
+        result = self.service.spreadsheets().values().get(
+            spreadsheetId=self.spreadsheet_id, range=sheet_title).execute()
+        return result.get('values', [])
+
+    def get_title(self) -> str:
+        return self.sheet_metadata.get('properties', {}).get('title', '')
+
+
+def main():
+    sheets = Sheets(SERVICE_ACCOUNT_FILE)
+    writer = WhooshWriter()
+
+    for spreadsheet_id in [KATINO]:
+        document = sheets.get_document(spreadsheet_id)
+        locality = document.get_title()
+        sheet_titles = document.get_sheet_titles()
+        for title in sheet_titles:
+            print(f'processing sheet «{title}»')
+            rows = document.get_sheet_content(title)
+            if len(rows) < 2:
+                continue
+
+            try:
+                base_column = rows[0].index('Фамилия')
+            except ValueError:
+                base_column = 5
+            name_column = base_column + 7
+            year_column = name_column + 3
+            print(f'detected: name column {name_column}, year column {year_column}')
+
+            for rowidx, row in enumerate(rows[1:]):
+                print('processing row:', row)
+                if (len(row) < year_column + 1):
+                    print('no data')
+                    continue
+                name = row[name_column]
+                if not name:
+                    continue
+                year = row[year_column]
+                print('adding document:', name, year)
+                writer.add_document(sheet=title, row=rowidx+2, content=name, locality=locality, birth_year=year)
+
+    writer.close()
+
+    reader = WhooshReader(writer.index)
+    search_results = reader.search("Демид")
+    for result in search_results:
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
